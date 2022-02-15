41968031ad591fdbf147c2dd10f49090e34935ec
杉本階嗣 <>
2022-02-16 02:52:40 +0900

    feat: headingコンポーネント追加

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,39 @@
 <template lang="pug">
 artile
-  .condition
-    WeatherForecastCondition2
-  template(v-if="$store.getters['weatherForecastStore/weatherForecast'] != null")
-    .cards
-      template(v-for="(report) in $store.getters['weatherForecastStore/weatherForecast'].reports")
-        WeatherForecastCard(:report="report")
+  Heading(:value="headingValue")
+  .content
+    .condition
+      WeatherForecastCondition2
+    template(v-if="$store.getters['weatherForecastStore/weatherForecast'] != null")
+      .cards
+        template(v-for="(report) in $store.getters['weatherForecastStore/weatherForecast'].reports")
+          WeatherForecastCard(:report="report")
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Heading from '~/components/pages/common/Heading.vue'
 import WeatherForecastCondition2 from '~/components/pages/weatherForecast/WeatherForecastCondition2.vue'
 import WeatherForecastCard from '~/components/pages/weatherForecast/WeatherForecastCard.vue'
+
+interface DataType {
+  headingValue: string
+}
+
 export default Vue.extend({
 
   components: {
+    Heading,
     WeatherForecastCondition2,
     WeatherForecastCard,
+  },
+
+
+
+  data(): DataType {
+    return {
+      headingValue: "過去の天気予報を検索"
+    }
   },
 
   async fetch({ store }) {
@@ -28,7 +45,15 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.content {
+  width: 95%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .condition {
+  margin-top: 50px;
   margin-bottom: 20px;
 }
 .cards {
