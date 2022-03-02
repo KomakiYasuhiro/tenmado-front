bfe5128a1d8c572958292a8b6b631b2dc6750e19
杉本階嗣 <>
2022-03-02 23:34:12 +0900

    feat: 一応topページも作っておく ただしindexには乗せない

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+article
+    p メンテナンス中
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    data() {
+        return {}
+    },
+
+    head() {
+        return {
+            titleTemplate: '',
+            title: 'テンマド',
+            meta: [
+                { hid: "robots", name: "robots", content: "noindex" }
+            ]
+        }
+    }
+})
+</script>
